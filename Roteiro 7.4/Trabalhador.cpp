4bb408a7bb76aee6ed6be027860695cb3cb42407
weslleygomes <30627516+weslleygomes@users.noreply.github.com>
2017-10-03 11:02:21 -0300

    Update Trabalhador.cpp

--- Roteiro 7.4/Trabalhador.cpp
+++ Roteiro 7.4/Trabalhador.cpp
@@ -1,20 +1,21 @@
 #include "Trabalhador.h"
-
-float Trabalhador::calcularPagamento(int Horas, int x){
-	
-	if(x==1){
-		if(Horas<=40){
-			return salario1*Horas;
-		}else{
-			Horas = Horas - 40;
-			return (salario1*40)+(salario1*Horas*1.5);
-		}
-	}else{
-		if(Horas<=40){
-			return salario*Horas;	
-		}else{
-			return salario*40;	
-		}
-		
-	}
-}
+ using namespace std;
+ #include <iostream>
+ 
+ float Trabalhador::calcularPagamento(int Horas, int x){
+ 	
+ 	if(x==1){
+ 		if(Horas<=40){
+ 			return salario1*Horas;
+ 		}else{
+ 			return salario1*40;
+ 		}
+ 	}else{
+ 		if(Horas<=40){
+ 			return salario*Horas;	
+ 		}else{
+ 			Horas = Horas - 40;
+ 			return (salario*40)+(salario*Horas*1.5);
+ 		}
+ 	}
+ }
